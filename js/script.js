6361b8768200f8ecbc879ee878eff0a179966224
God'swill Sekav Apya <godswillapya@gmail.com>
2024-09-04 17:55:29 +0100

    Information Desk Page Build

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// ACTIVE NAVIGATION STATE
+
+// const navLinkEls = document.querySelectorAll('.nav__link');
+
+// navLinkEls.forEach(navLinkEl => {
+//     navLinkEl.addEventListener('click', () => {
+//         document.querySelector('.active')?.classList.remove('active');
+//         navLinkEl.classList.add('active');
+//     });
+// });
