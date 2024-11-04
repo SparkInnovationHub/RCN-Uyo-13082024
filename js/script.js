3f24bd3b36cbbc1e0d28c2bf2f504417e11e5452
God'swill Sekav Apya <godswillapya@gmail.com>
2024-11-04 16:16:29 +0100

    Responsive Design Complete Revamp

--- js/script.js
+++ js/script.js
@@ -8,3 +8,12 @@
 //         navLinkEl.classList.add('active');
 //     });
 // });
+
+const numbers = [123, 25, 78, 5, 9];
+
+// Filter numbers less than 10 and find their indexes
+const indexes = numbers
+    .map((num, index) => (num < 10 ? index : -1)) // Map indexes of numbers less than 10
+    .filter(index => index !== -1); // Filter out -1 values to get the actual indexes
+
+console.log(indexes); // Output: [3, 4]
