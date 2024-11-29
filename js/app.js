cdd8ea114e97f964b401ea7c6ffde7e7ab570b9c
Igoche Anthony <54316737+OdehIgoche@users.noreply.github.com>
2024-11-29 06:12:07 +0100

    Correction of Active state at initial loading of page

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
+// const activePage = window.location.pathname;
+// const navLink = document.querySelectorAll('.deskLink').forEach(link => {
+//     if(link.href.includes(`${activePage}`)){
+//         link.classList.add('active');
+//     }
+// })
 const activePage = window.location.pathname;
-const navLink = document.querySelectorAll('.deskLink').forEach(link => {
-    if(link.href.includes(`${activePage}`)){
+document.querySelectorAll('.deskLink').forEach(link => {
+    if(link.pathname === activePage) {
         link.classList.add('active');
     }
-})
+});
+// const activePage = window.location.pathname;
+// document.querySelectorAll('.deskLink').forEach(link => {
+//     if(link.href.includes(activePage)) {
+//         link.classList.add('active');
+//     }
+// });
